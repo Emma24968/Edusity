6aa4245b8dc0a6fda33ed964a0ee06cad50df954
Emma24968 <emnabs12@gmail.com>
2026-02-26 02:15:48 +0100

    Added logo

--- my-react-app/src/components/navbar/Navbar.jsx
+++ my-react-app/src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 function Navbar() {
   return (
    <nav>
+    <div><img src={logo} alt="logo" width={50} /></div>
    </nav>
   )
 }
