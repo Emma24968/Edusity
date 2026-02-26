cd9a98783a8b74d2abfb2479a425514dc4976be1
Emma24968 <emnabs12@gmail.com>
2026-02-26 15:12:09 +0100

    Added the nav section

--- my-react-app/src/components/navbar/Navbar.jsx
+++ my-react-app/src/components/navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import logo from '../../assets/logo.png'
+import React from "react";
+import logo from "../../assets/logo.png";
 
 function Navbar() {
   return (
-   <nav>
-    <div><img src={logo} alt="logo" width={50} /></div>
-   </nav>
-  )
+    <nav className="bg-[#413D66] h-[100vh]">
+      <img src={logo} alt="logo" width={50} />
+      <ul className="text-white">
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonial</li>
+        <li><button>Contact</button></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
