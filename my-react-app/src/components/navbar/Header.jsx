75e5ce93abb99139d865c8da3731fd8992a25566
Emma24968 <emnabs12@gmail.com>
2026-02-26 16:38:47 +0100

    Added the header and hero components

---
+++ my-react-app/src/components/navbar/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <h3>
+        We Ensure better education <br /> for a better world
+      </h3>
+      <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, <br /> and experiences needed to excel in the dynamic field of education</p>
+    </div>
+  );
+}
