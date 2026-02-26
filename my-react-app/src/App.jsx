dff7c9b9c3e43ca345b607a9d8682d709c28e174
Emma24968 <emnabs12@gmail.com>
2026-02-26 16:13:13 +0100

    Added a background image

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
-  return <Navbar />;
+  return(
+    <div className="pl-[10%] pr-[10%]">
+      <Navbar />
+    </div>
+  ) ;
 };
 
 export default App;
--- my-react-app/src/components/navbar/Navbar.jsx
+++ my-react-app/src/components/navbar/Navbar.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import hero from "../../assets/hero.png";
 
 function Navbar() {
   return (
-    <nav className="bg-[#413D66] fixed top-0 w-[100%] justify-between items-center px-[4px] py-0 left-0 flex  ">
-      <img src={logo} alt="logo" />
-      <ul className="text-white flex gap-6">
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonial</li>
-        <li>
-          <button>Contact</button>
-        </li>
-      </ul>
+    <nav className="fixed top-0 left-0 w-full h-screen bg-[#413D66] overflow-hidden">
+      <img
+        src={hero}
+        alt="hero"
+        className="absolute inset-0 w-full h-full object-cover opacity-40"
+      />
+
+      {/* Overlay Color (optional but recommended) */}
+      <div className="absolute inset-0 bg-[#413D66]/70"></div>
+
+      <div className="relative z-10 flex justify-between items-center px-[10%] py-4">
+        <img src={logo} alt="logo" width={180} />
+
+        <ul className="text-white flex items-center gap-6">
+          <li>Home</li>
+          <li>Program</li>
+          <li>About us</li>
+          <li>Campus</li>
+          <li>Testimonial</li>
+          <li>
+            <button className="bg-white text-black p-2 rounded-full">
+              Contact
+            </button>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
