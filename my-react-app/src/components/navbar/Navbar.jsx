b9c86af7e8df24f3482f14e95efe26daecd56376
Emma24968 <emnabs12@gmail.com>
2026-02-26 15:30:22 +0100

    Styled the nave section

--- my-react-app/src/components/navbar/Navbar.jsx
+++ my-react-app/src/components/navbar/Navbar.jsx
@@ -3,15 +3,17 @@ import logo from "../../assets/logo.png";
 
 function Navbar() {
   return (
-    <nav className="bg-[#413D66] h-[100vh]">
-      <img src={logo} alt="logo" width={50} />
-      <ul className="text-white">
+    <nav className="bg-[#413D66] fixed top-0 w-[100%] justify-between items-center px-[4px] py-0 left-0 flex  ">
+      <img src={logo} alt="logo" />
+      <ul className="text-white flex gap-6">
         <li>Home</li>
         <li>Program</li>
         <li>About us</li>
         <li>Campus</li>
         <li>Testimonial</li>
-        <li><button>Contact</button></li>
+        <li>
+          <button>Contact</button>
+        </li>
       </ul>
     </nav>
   );
